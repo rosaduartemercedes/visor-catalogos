ca7f4104d49798fdf51a52484117908b39ffe9ee
rosaduartemercedes <rosa.duarte.mercedes@gmail.com>
2026-05-16 18:17:54 -0300

    middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,16 +1,16 @@
-/*import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const userAgent = request.headers.get('user-agent') || '';
 
   // Si el que entra es el robot de Meta, le metemos headers para limpiar el camino
-  if (userAgent.includes('facebookexternalhit') || userAgent.includes('Facebot')) {
+/*  if (userAgent.includes('facebookexternalhit') || userAgent.includes('Facebot')) {
     const response = NextResponse.next();
     response.headers.set('Access-Control-Allow-Origin', '*');
     response.headers.set('Cache-Control', 'no-store, max-age=0');
     return response;
-  }
+  }*/
 
   return NextResponse.next();
 }
@@ -18,4 +18,4 @@ export function middleware(request: NextRequest) {
 // Solo aplicamos este intermediario a las rutas de los catálogos
 export const config = {
   matcher: '/ver/:path*',
-};*/
+};
